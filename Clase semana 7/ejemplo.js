3b75d989849e820fd87b9d16e31f7c6408376f1a
Nicolasotelo <157720452+Nicolasotelo@users.noreply.github.com>
2025-09-26 14:20:17 -0500

    Nada

---
+++ Clase semana 7/ejemplo.js
@@ -0,0 +1,16 @@
+//Variables
+let nombre="Andres";
+//contante
+const universidad ="Universidad de los Andes"
+//Un usuario está dado de alta en el sitio web
+var isActive = true;
+//Numero
+let size=100;
+//String
+let nombreCurso = "Alfabetismo web"
+//Arrays
+[1,2,3,4,5]
+//Diccionario
+let usuario = {name:"Pedro", age:25}
+//Funciones
+//Condicionales
